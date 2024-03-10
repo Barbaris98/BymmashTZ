d1da712946855e3e8647a5ed035848d96f96bfce
Dmitry Grigorev <d-d12@mail.ru>
2024-03-10 23:08:20 +0400

    Готово, но без сферичности

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,36 +18,92 @@ namespace BymmashTZ.Controllers
             const decimal Yd_ves = 0.0000000078M;
 
             // считаем припуск 
-            decimal H_Zagotovki = Convert.ToDecimal(number_H + number_Pripysk_d);
-            decimal D_Zagotovki = Convert.ToDecimal(number_D + number_Pripysk_d);
-            decimal d_Zagotovki = Convert.ToDecimal(number__d - number_Pripysk_d);
+            decimal H_Zagotovki_Nominal = Convert.ToDecimal(number_H + number_Pripysk_d);
+            decimal H_Zagotovki_MAX = Convert.ToDecimal(number_H + number_Pripysk_d + number_Dopysk_delta);
+            decimal H_Zagotovki_MIN = Convert.ToDecimal(number_H + number_Pripysk_d - number_Dopysk_delta);
 
-            decimal Radius_pokovki_Nominal = Convert.ToDecimal(D_Zagotovki/2);
-            decimal Radius_pokovki_MAX = Convert.ToDecimal(D_Zagotovki/2 + number_Dopysk_delta);
-            decimal Radius_pokovki_MIN = Convert.ToDecimal(D_Zagotovki/2 - number_Dopysk_delta);
+            decimal D_Zagotovki_Nominal = Convert.ToDecimal(number_D + number_Pripysk_d);
+            decimal D_Zagotovki_MAX = Convert.ToDecimal(number_D + number_Pripysk_d + number_Dopysk_delta);
+            decimal D_Zagotovki_MIN = Convert.ToDecimal(number_D + number_Pripysk_d - number_Dopysk_delta);
 
-            decimal Radius_OTV_pokovki_Nominal = Convert.ToDecimal(d_Zagotovki/2);
-            decimal Radius_OTV_pokovki_MAX = Convert.ToDecimal(d_Zagotovki/2 + number_Dopysk_delta);
-            decimal Radius_OTV_pokovki_MIN = Convert.ToDecimal(d_Zagotovki/2 - number_Dopysk_delta);
+            decimal d__Zagotovki_Nominal = Convert.ToDecimal(number__d - number_Pripysk_d);
+            decimal d__Zagotovki_MAX = Convert.ToDecimal((number__d - number_Pripysk_d) - number_Dopysk_delta);
+            decimal d__Zagotovki_MIN = Convert.ToDecimal(number__d + number_Pripysk_d + number_Dopysk_delta);
 
-            decimal Obem_Zagotovki_MAX = pi * (Radius_pokovki_MAX * Radius_pokovki_MAX) * H_Zagotovki;
-            decimal Obem_Zagotovki_MIN = pi * (Radius_pokovki_MIN * Radius_pokovki_MIN) * H_Zagotovki;
+            decimal Radius_pokovki_Nominal = Convert.ToDecimal(D_Zagotovki_Nominal / 2);
+            decimal Radius_pokovki_MAX = Convert.ToDecimal(D_Zagotovki_MAX / 2);
+            decimal Radius_pokovki_MIN = Convert.ToDecimal(D_Zagotovki_MIN / 2);
 
-            decimal Obem_OTV_Zagotovki_MAX = pi * (Radius_OTV_pokovki_MAX * Radius_OTV_pokovki_MAX) * H_Zagotovki;
-            decimal Obem_OTV_Zagotovki_MIN = pi * (Radius_OTV_pokovki_MIN * Radius_OTV_pokovki_MIN) * H_Zagotovki;
+            decimal Radius_OTV_pokovki_Nominal = Convert.ToDecimal(d__Zagotovki_Nominal / 2);
+            decimal Radius_OTV_pokovki_MAX = Convert.ToDecimal(d__Zagotovki_MAX / 2);
+            decimal Radius_OTV_pokovki_MIN = Convert.ToDecimal(d__Zagotovki_MIN / 2);
 
+            decimal Obem_Zagotovki_Nominal = pi * (Radius_pokovki_Nominal * Radius_pokovki_Nominal) * H_Zagotovki_Nominal;
+            decimal Obem_Zagotovki_MAX = pi * (Radius_pokovki_MAX * Radius_pokovki_MAX) * H_Zagotovki_MAX;
+            decimal Obem_Zagotovki_MIN = pi * (Radius_pokovki_MIN * Radius_pokovki_MIN) * H_Zagotovki_MIN;
+
+            decimal Obem_OTV_Zagotovki_Nominal = pi * (Radius_OTV_pokovki_Nominal * Radius_OTV_pokovki_Nominal) * H_Zagotovki_Nominal;
+            decimal Obem_OTV_Zagotovki_MAX = pi * (Radius_OTV_pokovki_MAX * Radius_OTV_pokovki_MAX) * H_Zagotovki_MAX;
+            decimal Obem_OTV_Zagotovki_MIN = pi * (Radius_OTV_pokovki_MIN * Radius_OTV_pokovki_MIN) * H_Zagotovki_MIN;
+
+            decimal Massa_zagotovki_Nominal = Yd_ves * Obem_Zagotovki_Nominal;
             decimal Massa_zagotovki_MAX = Yd_ves * Obem_Zagotovki_MAX;
             decimal Massa_zagotovki_MIN = Yd_ves * Obem_Zagotovki_MIN;
 
+            decimal Massa_OTV_zagotovki_Nominal = Yd_ves * Obem_OTV_Zagotovki_Nominal;
             decimal Massa_OTV_zagotovki_MAX = Yd_ves * Obem_OTV_Zagotovki_MAX;
             decimal Massa_OTV_zagotovki_MIN = Yd_ves * Obem_OTV_Zagotovki_MIN;
 
+            decimal Massa_zagotovki_Obshaya_Nominal = Massa_zagotovki_Nominal - Massa_OTV_zagotovki_Nominal;
             decimal Massa_zagotovki_Obshaya_MAX = Massa_zagotovki_MAX - Massa_OTV_zagotovki_MAX;
             decimal Massa_zagotovki_Obshaya_MIN = Massa_zagotovki_MIN - Massa_OTV_zagotovki_MIN;
 
-            
 
-            return Redirect("Home/Rezult");
+
+            ViewBag.D_Zagotovki_MIN = D_Zagotovki_MIN;
+            ViewBag.D_Zagotovki_Nominal = D_Zagotovki_Nominal;
+            ViewBag.D_Zagotovki_MAX = D_Zagotovki_MAX;
+
+            ViewBag.d__Zagotovki_MIN = d__Zagotovki_MIN;
+            ViewBag.d__Zagotovki_Nominal = d__Zagotovki_Nominal;
+            ViewBag.d__Zagotovki_MAX = d__Zagotovki_MAX;
+
+            ViewBag.Radius_pokovki_MIN = Radius_pokovki_MIN;
+            ViewBag.Radius_pokovki_Nominal = Radius_pokovki_Nominal;
+            ViewBag.Radius_pokovki_MAX = Radius_pokovki_MAX;
+
+            ViewBag.Radius_OTV_pokovki_MIN = Radius_OTV_pokovki_MIN;
+            ViewBag.Radius_OTV_pokovki_Nominal = Radius_OTV_pokovki_Nominal;
+            ViewBag.Radius_OTV_pokovki_MAX = Radius_OTV_pokovki_MAX;
+           
+            ViewBag.H_Zagotovki_MIN = H_Zagotovki_MIN;
+            ViewBag.H_Zagotovki_Nominal = H_Zagotovki_Nominal;
+            ViewBag.H_Zagotovki_MAX = H_Zagotovki_MAX;
+            /////////////////////////////////////////////
+            ViewBag.Obem_Zagotovki_MIN = Obem_Zagotovki_MIN;
+            ViewBag.Obem_Zagotovki_Nominal = Obem_Zagotovki_Nominal;
+            ViewBag.Obem_Zagotovki_MAX = Obem_Zagotovki_MAX;
+
+            ViewBag.Obem_OTV_Zagotovki_MIN = Obem_OTV_Zagotovki_MIN;
+            ViewBag.Obem_OTV_Zagotovki_Nominal = Obem_OTV_Zagotovki_Nominal;
+            ViewBag.Obem_OTV_Zagotovki_MAX = Obem_OTV_Zagotovki_MAX;
+
+
+            ViewBag.Massa_zagotovki_MIN = Massa_zagotovki_MIN;
+            ViewBag.Massa_zagotovki_Nominal = Massa_zagotovki_Nominal;
+            ViewBag.Massa_zagotovki_MAX = Massa_zagotovki_MAX;
+
+            ViewBag.Massa_OTV_zagotovki_MIN = Massa_OTV_zagotovki_MIN;
+            ViewBag.Massa_OTV_zagotovki_Nominal = Massa_OTV_zagotovki_Nominal;
+            ViewBag.Massa_OTV_zagotovki_MAX = Massa_OTV_zagotovki_MAX;
+
+            ViewBag.Massa_zagotovki_Obshaya_MIN = Massa_zagotovki_Obshaya_MIN;
+            ViewBag.Massa_zagotovki_Obshaya_Nominal = Massa_zagotovki_Obshaya_Nominal;
+            ViewBag.Massa_zagotovki_Obshaya_MAX = Massa_zagotovki_Obshaya_MAX;
+
+
+            return View("Rezult");
+            //return Redirect("Home/Rezult");
 
         }
 
