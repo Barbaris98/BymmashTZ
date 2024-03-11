6ce29f17cced73e1f2d9f654e21a9dc57221ea66
Dmitry Grigorev <d-d12@mail.ru>
2024-03-11 21:34:54 +0400

    Готово

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -58,7 +58,21 @@ namespace BymmashTZ.Controllers
             decimal Massa_zagotovki_Obshaya_MAX = Massa_zagotovki_MAX - Massa_OTV_zagotovki_MAX;
             decimal Massa_zagotovki_Obshaya_MIN = Massa_zagotovki_MIN - Massa_OTV_zagotovki_MIN;
 
+            //сферичность
+            decimal Sverichnost_Nominal = (0.000000393M * (((D_Zagotovki_Nominal - d__Zagotovki_Nominal) +
+                (number_Pripysk_d * 2)) * (pi * Radius_pokovki_Nominal * Radius_pokovki_Nominal))) / 10000;
 
+            decimal Sverichnost_MIN = (0.000000393M * (((D_Zagotovki_MIN - d__Zagotovki_MIN) +
+                (number_Pripysk_d * 2)) * (pi * Radius_pokovki_MIN * Radius_pokovki_MIN))) / 10000;
+
+            decimal Sverichnost_MAX = (0.000000393M * (((D_Zagotovki_MAX - d__Zagotovki_MAX) +
+                (number_Pripysk_d * 2)) * (pi * Radius_pokovki_MAX * Radius_pokovki_MAX))) / 10000;
+
+            // Масса общая с учёт сверичности
+
+            decimal Massa_zagotovki_Obshaya_Sverich_Nominal = Sverichnost_Nominal + Massa_zagotovki_Obshaya_Nominal;
+            decimal Massa_zagotovki_Obshaya_Sverich_MAX = Sverichnost_MAX + Massa_zagotovki_Obshaya_MAX;
+            decimal Massa_zagotovki_Obshaya_Sverich_MIN = Sverichnost_MIN + Massa_zagotovki_Obshaya_MIN;
 
             ViewBag.D_Zagotovki_MIN = D_Zagotovki_MIN;
             ViewBag.D_Zagotovki_Nominal = D_Zagotovki_Nominal;
@@ -101,6 +115,14 @@ namespace BymmashTZ.Controllers
             ViewBag.Massa_zagotovki_Obshaya_Nominal = Massa_zagotovki_Obshaya_Nominal;
             ViewBag.Massa_zagotovki_Obshaya_MAX = Massa_zagotovki_Obshaya_MAX;
 
+            //
+            ViewBag.Sverichnost_Nominal = Sverichnost_Nominal;
+            ViewBag.Sverichnost_MIN = Sverichnost_MIN;
+            ViewBag.Sverichnost_MAX = Sverichnost_MAX;
+
+            ViewBag.Massa_zagotovki_Obshaya_Sverich_MIN = Massa_zagotovki_Obshaya_Sverich_MIN;
+            ViewBag.Massa_zagotovki_Obshaya_Sverich_Nominal = Massa_zagotovki_Obshaya_Sverich_Nominal;
+            ViewBag.Massa_zagotovki_Obshaya_Sverich_MAX = Massa_zagotovki_Obshaya_Sverich_MAX;
 
             return View("Rezult");
             //return Redirect("Home/Rezult");
